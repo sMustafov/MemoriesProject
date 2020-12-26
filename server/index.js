1882fe344b1e2c69c9ff934cff887a279ab818ef
Sevgin Mustafov <sevgin95@gmail.com>
2020-12-26 12:41:54 +0200

    Provide Redux functionality

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ const app = express();
 
 dotenv.config();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors);
- 
+
+app.use('/posts', postRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
