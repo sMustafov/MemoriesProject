8b9d0f29f56ff4511243d0734fa2dfb18662c271
Sevgin Mustafov <sevgin95@gmail.com>
2020-12-29 12:48:19 +0200

    Added delete functionality for posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,11 +4,14 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons//MoreHoriz';
 import moment from 'moment';
-
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     return (
         <Card className={classes.card}>
@@ -44,7 +47,7 @@ const Post = ({ post, setCurrentId }) => {
                     {post.likeCount}
                 </Button>
 
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize='small' />
                     Delete
                 </Button>
